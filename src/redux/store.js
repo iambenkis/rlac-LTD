fcadf288edb21595eff54b28ad03474b795ef8c8
benkis <benkisenge03@gmail.com>
2023-03-02 14:39:39 +0200

    Create the store

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import {combineReducers} from "redux";
+import {firebaseReducer} from "react-redux-firebase";
+import {firestoreReducer} from "redux-firestore";
+import { legacy_createStore as createStore } from "redux";
+import signupReducer from "./signup";
+import loginReducer from "./login";
+import dataReducer from "./display";
+
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const rootReducer = combineReducers({
+    signup : signupReducer,
+    login : loginReducer,
+    data: dataReducer
+});
+ 
+
+export const store = createStore(rootReducer, reduxDevtools)
