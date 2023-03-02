1345f4e5263c047d6b5eeebea5937b21a9beb932
benkis <benkisenge03@gmail.com>
2023-03-02 14:38:09 +0200

    Add firebase config file

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -14,18 +14,3 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const AuthContext = createContext();
-export const AuthContextProvider = props => {
-    const [user, setUser] = useState()
-    const [error, setError] = useState()
-  
-    useEffect(() => {
-      const unsubscribe = onAuthStateChanged(getAuth(), setUser, setError)
-      return () => unsubscribe()
-    }, [])
-    return <AuthContext.Provider value={{ user, error }} {...props} />
-}
-export const useAuthState = () => {
-    const auth = useContext(AuthContext)
-    return { ...auth, isAuthenticated: auth.user != null }
-}
