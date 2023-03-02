378382e58e3b758274da102419e33b908d94b964
benkis <benkisenge03@gmail.com>
2023-03-02 14:40:04 +0200

    Add sign up component

--- src/authentification/signup.js
+++ src/authentification/signup.js
@@ -1,67 +1,47 @@
-import React, { useContext, useRef } from 'react'; 
+import React, { useRef } from 'react'; 
 import { Link } from 'react-router-dom';
 import  appConfig  from '../data/firebase';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth' 
 import { AuthContext } from '../data/firebase';
+import { useFirebase } from "react-redux-firebase"; 
+import { useSelector, useStore } from 'react-redux';
+import { fetchSignupAuth } from '../redux/signup';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; 
 
-import {
-    useLocation,
-    useNavigate,
-    useParams,
-  } from "react-router-dom";
-  
-  function withRouter(Component) {
-    function ComponentWithRouterProp(props) {
-      let location = useLocation();
-      let navigate = useNavigate();
-      let params = useParams();
-      return (
-        <Component
-          {...props}
-          router={{ location, navigate, params }}
-        />
-      );
-    }
-  
-    return ComponentWithRouterProp;
-  }
-
-const Signup = ({history}) => {
+const Signup = () => { 
     const emailRef = useRef();
     const passwordRef = useRef();
     const auth = getAuth();
+    const navigate = useNavigate(); 
+    const store = useStore();
+    const state = useSelector(state => state.signup)
+
     const handleSignup = (e) => {
         e.preventDefault(); 
-        createUserWithEmailAndPassword( auth, emailRef.current.value, passwordRef.current.value )
-        .then((cred) => {
-            console.log(cred.user)
-            e.target.reset();
-        })
-        .catch((err) => {
-            console.log(err.message)
-            e.target.reset();
-        }) 
-    }
+        fetchSignupAuth(auth, emailRef.current.value,passwordRef.current.value, store)
+        e.target.reset();
+    }  
+    if (state.status) {
+        navigate('/login');
+        localStorage.removeItem("signed")
+    }  
 
-    // const { currentUser } = useContext(AuthContext);
-
- console.log("User : ", useContext(AuthContext))
-    
     return (
         <div className="signup">
             <div> 
                 <h2>Sign up</h2>
                 <form onSubmit={handleSignup}>
                     <label for="username">Username:</label><br />
-                    <input ref={emailRef} type="text" id="username" name="username" /><br />
+                    <input ref={emailRef} type="text" id="username" name="username" required/><br />
                     <label for="pwd">Password:</label><br />
-                    <input ref={passwordRef} type="password" id="pwd" name="pwd" /><br />
+                    <input ref={passwordRef} type="password" id="pwd" name="pwd" required/><br />
                     <button type="submit">Submit</button>
                 </form>
-                {/* <Link to='/signUp' >sign up</Link> */}
+                <p>Already have an account ? <Link to='/login' >Log in</Link></p>
             </div>
         </div>
     );
 }
 
-export default withRouter(Signup);
+export default Signup;
