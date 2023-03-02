d34cc477438fea0014f5f37b9e35e71a48cb0d26
benkis <benkisenge03@gmail.com>
2023-03-02 14:37:08 +0200

    Retrieve data to be displayed

--- src/components/data-display.js
+++ src/components/data-display.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect} from "react";
 import  appConfig  from '../data/firebase';
 import { getDatabase, ref,  onValue  } from "firebase/database";
+import { useSelector, useStore  } from "react-redux";
+import { getData } from "../redux/display"; 
 
 const Display = () => {
     const realDb = getDatabase();
     const bookRef = ref(realDb);
-    let obj = {};
+    let obj = {}; 
+    const store = useStore();
+    const data = useSelector(state => state.data)
 
-    useEffect(() => {
-        onValue(bookRef, snapshot => {
-            obj = snapshot.val(); 
-            console.log(obj);
-        })
-    },[])
-
+    useEffect(() => { 
+        getData(bookRef, store)
+        console.log(data, "myData")
+    },[data])
+    
+    
     return (
-        <h1>Data here</h1>
+        <>
+            <h1>Data here</h1>
+            <ul> 
+              <li>data</li>
+            </ul>
+        </>
     )
 }
 export default Display;
