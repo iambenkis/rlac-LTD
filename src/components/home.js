8814e68e75b416d2928d48b12d3abca91e705c0f
joel-kalema <joelkalema63@gmail.com>
2023-09-14 02:30:16 +0200

    add images

--- src/components/home.js
+++ src/components/home.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Command from './command-page'
 import './style.css';
 
+
 const Home = () => {
   return (
     <div className='home'>
       <Command />
+      <div className='home-image'>
+        <img src='log.png' alt='home image' />
+      </div>
     </div>
   )
 }
